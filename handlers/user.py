1990d36a788f306dcf7dc78b14ed1e2efb3e81ae
piyushraj0284-glitch <piyushraj0284@gmail.com>
2026-03-31 10:05:08 +0530

    Update user.py

--- handlers/user.py
+++ handlers/user.py
@@ -1,20 +1,38 @@
-msg = await messages.insert_one({
-    "user_id": message.from_user.id,
-    "text": message.text,
-    "timestamp": message.date.timestamp()
-})
+from aiogram import Router, types
+from db import users, messages
+from config import ADMIN_IDS
 
-msg_id = str(msg.inserted_id)
+router = Router()
 
-for admin in ADMIN_IDS:
-    sent = await message.bot.forward_message(
-        chat_id=admin,
-        from_chat_id=message.chat.id,
-        message_id=message.message_id
+@router.message()
+async def handle_user(message: types.Message):
+    if message.from_user.id in ADMIN_IDS:
+        return
+
+    # Save user
+    await users.update_one(
+        {"user_id": message.from_user.id},
+        {"$set": {"user_id": message.from_user.id}},
+        upsert=True
     )
 
-    # Save mapping
-    await messages.update_one(
-        {"_id": msg.inserted_id},
-        {"$set": {"admin_msg_id": sent.message_id}}
-    )
+    # Save message
+    msg = await messages.insert_one({
+        "user_id": message.from_user.id,
+        "text": message.text,
+        "timestamp": message.date.timestamp()
+    })
+
+    # Forward message to admin
+    for admin in ADMIN_IDS:
+        sent = await message.bot.forward_message(
+            chat_id=admin,
+            from_chat_id=message.chat.id,
+            message_id=message.message_id
+        )
+
+        # Save mapping
+        await messages.update_one(
+            {"_id": msg.inserted_id},
+            {"$set": {"admin_msg_id": sent.message_id}}
+        )
