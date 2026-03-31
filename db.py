35db5fe3f0b6b615d74138f144774ade6aec2023
piyushraj0284-glitch <piyushraj0284@gmail.com>
2026-03-31 09:12:22 +0530

    Create db.py

---
+++ db.py
@@ -0,0 +1,8 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import MONGO_URI
+
+client = AsyncIOMotorClient(MONGO_URI)
+db = client["livegram_pro"]
+
+users = db["users"]
+messages = db["messages"]
