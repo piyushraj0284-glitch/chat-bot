4b358867f10524ec4d94485e1e48a38a1a5ced6c
piyushraj0284-glitch <piyushraj0284@gmail.com>
2026-04-01 19:28:30 +0530

    Delete db.py

--- db.py
+++
@@ -1,8 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-from config import MONGO_URI
-
-client = AsyncIOMotorClient(MONGO_URI)
-db = client["livegram_pro"]
-
-users = db["users"]
-messages = db["messages"]
